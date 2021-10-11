f00f97d74b909b4ae9998fd3abb6b5dfecfbb6d5
Dmytro Tereshchenko <legat4life@gmail.com>
2021-10-11 22:33:01 +0300

    Add project files.

---
+++ MiniShop/Util/AutofacConfig.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiniShop.Util
+{
+    public class AutofacConfig
+    {
+        public static void ConfigureContainer()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+            /*builder.RegisterType<BookRepository>().As<IRepository>();*/
+            var container = builder.Build();
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
